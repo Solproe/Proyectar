8a895c22b495a0627f00a826ea289fbd3338f7bb
domainKT01 <luisdanielcoronelposada903@gmail.com>
2023-02-01 20:08:39 -0500

    new data

--- public/js/createElements.js
+++ public/js/createElements.js
@@ -1,11 +1,11 @@
-function createNewElement(data) {
-    const newElement = document.createElement("div");
-    newElement.classList.add("div");
-    newElement.textContent = data;
-    document.querySelector(".container").appendChild(newElement);
+function createNewElement(data, num) {
+    const tbody = document.getElementById("tbody");
+    alert(data.name);
+    tbody.innerHTML += '<tr><th scope="row">1</th><td>' + data.name + '</td><td>' + data.status + '</td></tr>';
 }
 
 function getAllRequests() {
+    var num = 1;
     var ws = new WebSocket("ws://trusting-cultured-ceratonykus.glitch.me");
     ws.onopen = function(evt) {
         var matriz = ["JPY956. MOVIL.11", "EHN881 MOVIL.06"];
@@ -20,8 +20,9 @@ function getAllRequests() {
         alert("close");
     }
     ws.onmessage = function(evt) {
-        var data = evt.data;
-        createNewElement(data);
+        var data = JSON.parse(evt.data);
+        createNewElement(data, num);
+        num += 1;
     }
 }
 
